94b46e601fad477d355d49370c85d424a8628670
YuMiinZ <yumiin00@hotmail.com>
2023-12-18 05:05:45 -0600

    Principal, proveedor, empleado

--- src/main/java/Vista/JF_RegistrarProveedor.java
+++ src/main/java/Vista/JF_RegistrarProveedor.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionListener;
  *
  * @author yumii
  */
-public class JF_Principal extends javax.swing.JFrame {
+public class JF_RegistrarProveedor extends javax.swing.JFrame {
     private boolean menuAbierto = false;
     
     private JPopupMenu menu = new JPopupMenu();
@@ -26,7 +26,7 @@ public class JF_Principal extends javax.swing.JFrame {
     /**
      * Creates new form JF_Principal
      */
-    public JF_Principal() {
+    public JF_RegistrarProveedor() {
         initComponents();
         customComponents();
         eventComponents();
@@ -34,6 +34,7 @@ public class JF_Principal extends javax.swing.JFrame {
         cerrarMenu();
         
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
     }
 
     /**
@@ -46,20 +47,49 @@ public class JF_Principal extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        lblFondo = new javax.swing.JLabel();
+        lblTelefono = new javax.swing.JLabel();
+        lblNombre = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        btnAgregar = new javax.swing.JButton();
+        lblTitulo = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         btnMenu = new javax.swing.JButton();
         lblCotosFood = new javax.swing.JLabel();
+        txtTelefono = new javax.swing.JTextField();
+        btnRegresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1747, 1291));
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        lblFondo.setBackground(new java.awt.Color(255, 255, 255));
-        lblFondo.setMaximumSize(new java.awt.Dimension(3000, 3000));
-        lblFondo.setMinimumSize(new java.awt.Dimension(1747, 1186));
-        jPanel1.add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 53, 1750, 1310));
+        lblTelefono.setFont(new Font ("Montserrat", Font.BOLD,36));
+        lblTelefono.setText("Telefono");
+        jPanel1.add(lblTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 450, 470, -1));
+
+        lblNombre.setFont(new Font ("Montserrat", Font.BOLD,36));
+        lblNombre.setText("Nombre");
+        jPanel1.add(lblNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 290, 470, -1));
+
+        txtNombre.setFont(new Font ("Montserrat", Font.PLAIN,26));
+        jPanel1.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 340, 470, 38));
+
+        btnAgregar.setBackground(new java.awt.Color(0, 72, 121));
+        btnAgregar.setFont(new Font ("Montserrat", Font.BOLD,30));
+        btnAgregar.setForeground(new java.awt.Color(255, 255, 255));
+        btnAgregar.setText("Agregar");
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 600, 199, 50));
+
+        lblTitulo.setFont(new Font("HeadlandOne", Font.BOLD, 64));
+        lblTitulo.setForeground(new java.awt.Color(0, 72, 121));
+        lblTitulo.setText("Registrar Proveedor");
+        jPanel1.add(lblTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 150, -1, -1));
 
         jPanel2.setBackground(new java.awt.Color(57, 145, 151));
         jPanel2.setMinimumSize(new java.awt.Dimension(1747, 105));
@@ -77,11 +107,11 @@ public class JF_Principal extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(32, 32, 32)
+                .addGap(29, 29, 29)
                 .addComponent(btnMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(lblCotosFood)
-                .addContainerGap(1599, Short.MAX_VALUE))
+                .addContainerGap(1602, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -91,10 +121,28 @@ public class JF_Principal extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(btnMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 60));
+
+        txtTelefono.setFont(new Font ("Montserrat", Font.PLAIN,26));
+        txtTelefono.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTelefonoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 500, 470, 38));
+
+        btnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/IconoRegresar.png"))); // NOI18N
+        btnRegresar.setBorderPainted(false);
+        btnRegresar.setContentAreaFilled(false);
+        btnRegresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegresarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnRegresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, 40, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,10 +158,24 @@ public class JF_Principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void txtTelefonoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTelefonoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTelefonoActionPerformed
+
+    private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegresarActionPerformed
+
     private void customComponents(){
-        setImageLabel(lblFondo, "src/main/resources/Imagenes/Fondo4.jpeg");
         setButtonIcon(btnMenu, "src/main/resources/Imagenes/IconoMenu.png");
-    }
+        setButtonIcon(btnRegresar, "src/main/resources/Imagenes/IconoRegresar.png");
+
+}
+
     
     private void eventComponents() {
         btnMenu.addActionListener(new ActionListener() {
@@ -146,20 +208,23 @@ public class JF_Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JF_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_RegistrarProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JF_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_RegistrarProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JF_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_RegistrarProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JF_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_RegistrarProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JF_Principal().setVisible(true);
+                new JF_RegistrarProveedor().setVisible(true);
             }
         });
     }
@@ -265,10 +330,6 @@ public class JF_Principal extends javax.swing.JFrame {
         btnMenu.setBorderPainted(false);
     }
     
-    protected void mostrarFondo(boolean mostrar) {
-        lblFondo.setVisible(mostrar);
-    }
-    
     private void setButtonIcon(JButton button, String imagePath){
         ImageIcon image = new ImageIcon(imagePath);
         Icon icon = new ImageIcon(image.getImage().getScaledInstance(button.getWidth(), button.getHeight(), Image.SCALE_DEFAULT));
@@ -284,10 +345,16 @@ public class JF_Principal extends javax.swing.JFrame {
         this.repaint();
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgregar;
     private javax.swing.JButton btnMenu;
+    private javax.swing.JButton btnRegresar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel lblCotosFood;
-    private javax.swing.JLabel lblFondo;
+    private javax.swing.JLabel lblNombre;
+    private javax.swing.JLabel lblTelefono;
+    private javax.swing.JLabel lblTitulo;
+    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTextField txtTelefono;
     // End of variables declaration//GEN-END:variables
 }
